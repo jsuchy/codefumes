0b86500a467f1a3b90da308edd676c5122e8dfc6
Tom Kersten <tkersten@obtiva.com>
2009-05-12 19:05:49 -0500

    Added 'codometer' directory to load path - Updated spec.opts to run in specdoc format

--- lib/codometer.rb
+++ lib/codometer.rb
@@ -1,11 +1,17 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+def add_dir_to_load_path(dir_path)
+  $:.unshift(dir_path) unless
+    $:.include?(dir_path) || $:.include?(File.expand_path(dir_path))
+end
+
+add_dir_to_load_path(File.dirname(__FILE__))
+add_dir_to_load_path(File.dirname(__FILE__) + '/codometer')
 
 require 'rubygems'
 gem 'httparty', '>= 0.4.3'
 require 'httparty'
 
-require 'codometer/project'
+require 'project'
+require 'config_file'
 
 module Codometer
   VERSION = '0.0.1'

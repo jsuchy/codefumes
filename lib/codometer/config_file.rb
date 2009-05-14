8411b7ea3c6d3b2da3d3508339a09fd720b1512c
Tom Kersten <tkersten@obtiva.com>
2009-05-13 21:10:47 -0500

    Added ConfigFile#delete_project and refactored

--- lib/codometer/config_file.rb
+++ lib/codometer/config_file.rb
@@ -1,45 +1,52 @@
 module Codometer
-  class ConfigFile
+  module ConfigFile
+    extend self
+    DEFAULT_FILE_STRUCTURE = {}.dup
     DEFAULT_PATH = File.expand_path('~/.codometer_config')
-    DEFAULT_SKELETON_STRUCTURE = {}
 
     def self.path
       DEFAULT_PATH
     end
 
     def self.save_project(project)
-      write do
-        config = serialized
-        if config[:projects]
-          config[:projects].merge!(project.to_config)
-        else
-          config[:projects] = project.to_config
-        end
-        config
+      config = serialized
+      if config[:projects]
+        config[:projects].merge!(project.to_config)
+      else
+        config[:projects] = project.to_config
       end
+      write(config)
+    end
+
+    def self.delete_project(project)
+      config = serialized
+      config[:projects].delete(project.public_key.to_sym)
+      write(config)
     end
 
     def self.serialized
-      empty? ? DEFAULT_SKELETON_STRUCTURE : loaded
+      empty? ? DEFAULT_FILE_STRUCTURE : loaded
     end
 
-    private
-      def self.write(filename = DEFAULT_PATH, &block)
-        File.open(filename, 'w') do |f|
-          f.puts YAML::dump(yield) if block_given?
+    class << self
+      private
+        def write(serializable_object)
+          File.open(path, 'w') do |f|
+            f.puts YAML::dump(serializable_object)
+          end
         end
-      end
 
-      def self.exists?
-        File.exist?(DEFAULT_PATH)
-      end
+        def exists?
+          File.exists?(path)
+        end
 
-      def self.empty?
-        !(exists? && loaded.nil?)
-      end
+        def empty?
+          !(exists? && loaded)
+        end
 
-      def self.loaded
-        YAML::load_file(path)
-      end
+        def loaded
+          YAML::load_file(path)
+        end
+    end
   end
 end
--- spec/codometer/config_file_spec.rb
+++ spec/codometer/config_file_spec.rb
@@ -1,70 +1,94 @@
 require File.dirname(__FILE__) + '/../spec_helper.rb'
 
 describe "ConfigFile" do
-  context "class method" do
-    describe "calling 'path'" do
-      it "returns a the full path to a dotfile named '.codometer_config' in the user's home directory" do
-        ConfigFile.path.should == File.expand_path('~/.codometer_config')
+  before(:each) do
+    File.delete(ConfigFile.path) if File.exist?(ConfigFile.path)
+    @project = Project.new(:public_key => 'public_key_value', :private_key => 'private_key_value')
+  end
+
+  describe "calling 'path'" do
+    it "returns a the full path to a dotfile named '.codometer_config' in the user's home directory" do
+     ConfigFile.path.should == File.expand_path('~/.codometer_config')
+    end
+
+    context "when the file does not exist" do
+      before(:each) do
+        @file_path = ConfigFile.path
+      end
+    end
+  end
+
+  describe "calling 'save_project'" do
+    context "when passed a new project" do
+      it "creates the config file if it did not exist already" do
+        File.exist?(ConfigFile.path).should be_false
+        ConfigFile.save_project(@project)
+        File.exist?(ConfigFile.path).should be_true
       end
 
-      context "when the file does not exist" do
-        before(:each) do
-          @file_path = ConfigFile.path
-          File.delete(@file_path) if File.exist?(ConfigFile.path)
-        end
+      it "adds the supplied project's public key as a new entry under 'projects'" do
+        ConfigFile.save_project(@project)
+        ConfigFile.serialized[:projects].should include(@project.to_config)
       end
     end
 
-    describe "'save_project'" do
+    context "when passed an existing project" do
       before(:each) do
-        @project = Project.new(:public_key => 'ghKly', :private_key => 'a_private_key_here')
+        ConfigFile.save_project(@project)
+        @updated_project = Project.new(:public_key => @project.public_key, :private_key => "updated_private_key")
       end
 
-      context "when passed a new project" do
-        it "creates the config file if it did not exist already" do
-          File.delete(ConfigFile.path) if File.exist?(ConfigFile.path)
-          File.exist?(ConfigFile.path).should be_false
-          ConfigFile.save_project(@project)
-          File.exist?(ConfigFile.path).should be_true
-        end
+      it "does not create a duplicate entry in the list of projects" do
+        ConfigFile.serialized[:projects].should include(@project.to_config)
+        ConfigFile.serialized[:projects].count.should == 1
+        ConfigFile.save_project(@updated_project)
+        ConfigFile.serialized[:projects].should include(@updated_project.to_config)
+        ConfigFile.serialized.count.should == 1
+      end
+    end
 
-        it "adds the supplied project's public key as a new entry under 'projects'" do
-          ConfigFile.save_project(@project)
-          ConfigFile.serialized[:projects].should include(@project.to_config)
-        end
+    context "when several projects exist" do
+      before(:each) do
+        @project1 = Project.new(:public_key => :p1_pub_value, :private_key => "p1_private_key")
+        @project2 = Project.new(:public_key => :p2_pub_value, :private_key => "p2_private_key")
+        @project3 = Project.new(:public_key => :p3_pub_value, :private_key => "p3_private_key")
+        ConfigFile.save_project(@project1)
+        ConfigFile.save_project(@project2)
+        ConfigFile.save_project(@project3)
       end
 
-      context "when passed an existing project" do
-        before(:each) do
-          ConfigFile.save_project(@project)
-          @updated_project = Project.new(:public_key => @project.public_key, :private_key => "updated_private_key")
-        end
+      it "does not modify data pertaining to other projects" do
+        ConfigFile.serialized[:projects].should include(@project1.to_config)
+        ConfigFile.serialized[:projects].should include(@project2.to_config)
+        ConfigFile.serialized[:projects].should include(@project3.to_config)
+      end
+    end
+  end
 
-        it "does not create a duplicate entry in the list of projects" do
-          ConfigFile.serialized[:projects].should include(@project.to_config)
-          ConfigFile.serialized[:projects].count.should == 1
-          ConfigFile.save_project(@updated_project)
-          ConfigFile.serialized[:projects].should include(@updated_project.to_config)
-          ConfigFile.serialized.count.should == 1
-        end
+  describe "calling 'delete_project'" do
+    context "when the project entry exists in the file" do
+      before(:each) do
+        @project1 = Project.new(:public_key => "p1_pub_value", :private_key => "p1_private_key")
+        @project2 = Project.new(:public_key => "p2_pub_value", :private_key => "p2_private_key")
+        @project3 = Project.new(:public_key => "p3_pub_value", :private_key => "p3_private_key")
+        ConfigFile.save_project(@project1)
+        ConfigFile.save_project(@project2)
+        ConfigFile.save_project(@project3)
+        ConfigFile.delete_project(@project2)
       end
 
-      context "when several projects exist" do
-        before(:each) do
-          @project1 = Project.new(:public_key => "abcd", :private_key => "p1_private_key")
-          @project2 = Project.new(:public_key => "efgh", :private_key => "p2_private_key")
-          @project3 = Project.new(:public_key => "ijkl", :private_key => "p3_private_key")
-          ConfigFile.save_project(@project1)
-          ConfigFile.save_project(@project2)
-          ConfigFile.save_project(@project3)
-        end
-
-        it "does not modify data pertaining to other projects" do
-          ConfigFile.serialized[:projects].should include(@project1.to_config)
-          ConfigFile.serialized[:projects].should include(@project2.to_config)
-          ConfigFile.serialized[:projects].should include(@project3.to_config)
-        end
+      it "removes the entry for the supplied project from the file" do
+        ConfigFile.serialized[:projects].should_not include(@project2.to_config)
       end
+
+      xit "does not affect other project entries" do
+        ConfigFile.serialized[:projects].should include(@project1.to_config)
+        ConfigFile.serialized[:projects].should include(@project3.to_config)
+      end
+    end
+
+    context "when the project entry does not exist in the file" do
+      it "does not raise an error"
     end
   end
 end

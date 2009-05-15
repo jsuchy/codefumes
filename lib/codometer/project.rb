b3cf0db96cc0630ceed155d1602b719c9ce171ea
Tom Kersten <tkersten@obtiva.com>
2009-05-15 08:36:01 -0500

    Added short_uri, community_uri, api_uri accessors

--- lib/codometer/project.rb
+++ lib/codometer/project.rb
@@ -3,7 +3,7 @@ module Codometer
     include HTTParty
     base_uri 'http://www.codometer.net/api/v1/xml'
     format :xml
-    attr_accessor :public_key, :private_key
+    attr_accessor :public_key, :private_key, :short_uri, :community_uri, :api_uri
 
     def initialize(options = {})
       self.public_key = options[:public_key]
@@ -21,8 +21,11 @@ module Codometer
 
       case response.code
         when 201
-          self.public_key = response['project']['public_key']
-          self.private_key = response['project']['private_key']
+          self.public_key    = response['project']['public_key']
+          self.private_key   = response['project']['private_key']
+          self.short_uri     = response['project']['short_uri']
+          self.community_uri = response['project']['community_uri']
+          self.api_uri       = response['project']['api_uri']
           true
         else
           false
--- spec/codometer/project_spec.rb
+++ spec/codometer/project_spec.rb
@@ -5,26 +5,30 @@ describe "Project" do
     FakeWeb.clean_registry
   end
 
+  after(:each) do
+    FakeWeb.clean_registry
+  end
+
   context "creating a new project" do 
     context "successfully" do
       before(:each) do
         FakeWeb.register_uri( :post, "http://www.codometer.net/api/v1/xml/projects",
                               :status => ["201", "Created"],
-                              :string =>  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<project>\n  <access-secret nil=\"true\"></access-secret>\n  <created-at type=\"datetime\">2009-04-29T23:18:03Z</created-at>\n  <id type=\"integer\">1</id>\n  <public-key>foofoolerue</public-key>\n <private-key>foobarbaz</private-key>\n  <updated-at type=\"datetime\">2009-04-29T23:18:03Z</updated-at>\n</project>\n")
-      end
-      it "sets the public key" do
-        project = Codometer::Project.new
-        project.public_key.should be_nil
-        project.save
-        project.public_key.should_not be_nil
+                              :string =>  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<project>\n  <access-secret nil=\"true\"></access-secret>\n  <created-at type=\"datetime\">2009-04-29T23:18:03Z</created-at>\n  <id type=\"integer\">1</id>\n  <public-key>foofoolerue</public-key>\n <private-key>foobarbaz</private-key>\n  <updated-at type=\"datetime\">2009-04-29T23:18:03Z</updated-at>\n</project>\n <short_uri>http://www.codometer.net/p/foofoolerue</short_uri>\n <community_uri>http://www.codometer.net/community/projects/1</community_uri>\n <api-uri>http://www.codometer.net/api/v1/xml/projects/1.xml</api-uri>\n")
       end
 
-      it "sets the private key" do
-        project = Codometer::Project.new
-        project.private_key.should be_nil
-        project.save
-        project.private_key.should_not be_nil
-      end
+      [ :public_key,
+        :private_key,
+        :short_uri,
+        :community_uri,
+        :api_uri].each do |method_name|
+          it "sets the '#{method_name.to_s}'" do
+            project = Codometer::Project.new
+            project.send(method_name).should be_nil
+            project.save
+            project.send(method_name).should_not be_nil
+          end
+        end
     end
 
     context "unsuccessfully" do

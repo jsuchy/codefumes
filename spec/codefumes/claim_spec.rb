13d89f77e6f09b507f2b6d6474323c13c4524273
Tom Kersten <tom.kersten@cosyntech.com>
2009-09-21 00:17:08 -0500

    Updated claim to return true/false vs. instance

--- spec/codefumes/claim_spec.rb
+++ spec/codefumes/claim_spec.rb
@@ -12,24 +12,19 @@ describe Claim do
     @claim_uri = "http://www.codefumes.com/api/v1/xml/projects/#{@project.public_key}/claim?api_key=#{@api_key}"
   end
 
-  context "with 201 Created response" do
-    it "returns an instance of Claim with the created_at attribute set" do
-      @timestamp = "TIMESTAMP"
-      FakeWeb.register_uri( :post, @claim_uri,
-                            :status => ["201", "Created"],
-                            :string =>  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<claim>\n<created_at>#{@timestamp}</created_at></claim>\n")
-      claim = Claim.create(@project, @api_key)
-      claim.should be_instance_of(Claim)
-      claim.created_at.should == @timestamp
+  describe "create" do
+    context "with 201 Created response" do
+      it "returns an instance of Claim with the created_at attribute set" do
+        FakeWeb.register_uri( :post, @claim_uri, :status => ["201", "Created"], :string =>  "")
+        Claim.create(@project, @api_key).should be_true
+      end
     end
-  end
 
-  context "with Unauthorized response" do
-    it "returns nil" do
-      FakeWeb.register_uri( :post, @claim_uri,
-                            :status => ["401", "Unauthorized"],
-                            :string =>  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<claim>\n</claim>\n")
-      Claim.create(@project, @api_key).should == nil
+    context "with Unauthorized response" do
+      it "returns false" do
+        FakeWeb.register_uri( :post, @claim_uri, :status => ["401", "Unauthorized"], :string =>  "")
+        Claim.create(@project, @api_key).should be_false
+      end
     end
   end
 end

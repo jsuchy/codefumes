f09d477cc296032b46c45bd337608e70ea26aaed
Tom Kersten <tom.kersten@cosyntech.com>
2009-09-26 20:09:03 -0500

    Fixed spec (needed to delete file if existed)

--- spec/store_codefumes_credentials/cli_spec.rb
+++ spec/store_codefumes_credentials/cli_spec.rb
@@ -1,14 +1,19 @@
 require 'spec/spec_helper'
 require 'lib/store_codefumes_credentials/cli'
 
+def delete_config_file
+  unless ConfigFile.path == File.expand_path('~/.codefumes_config')
+    File.delete(ConfigFile.path) if File.exist?(ConfigFile.path)
+  end
+end
+
 describe StoreCodefumesCredentials::CLI, "execute" do
   after(:all) do
-    unless ConfigFile.path == File.expand_path('~/.codefumes_config')
-      File.delete(ConfigFile.path) if File.exist?(ConfigFile.path)
-    end
+    delete_config_file
   end
 
   before(:each) do
+    delete_config_file
     @api_key_value = "API_KEY#{rand(100)}"
     @stdout_io = StringIO.new
     StoreCodefumesCredentials::CLI.execute(@stdout_io, [@api_key_value])
